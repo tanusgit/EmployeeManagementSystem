29d5ae1d776aea9da79a0d65684c4981d58f32df
Tanusree Debnath <24976766+tanusgit@users.noreply.github.com>
2021-07-28 09:57:39 -0700

    used doget method

--- EmployeeManagement/src/main/java/com/example/EmployeeManagement/Add.java
+++ EmployeeManagement/src/main/java/com/example/EmployeeManagement/Add.java
@@ -1,93 +1,53 @@
 package com.example.EmployeeManagement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-import java.sql.*;
-class Oraclecon{
-    public static void main(String args[]){
-        try{
-            Employee employee = new Employee(3 , "adi", "tech", 2356455);
-            int id1 = employee.getId();
-            String name1 = employee.getName();
-            String dep1 = employee.getDepartment();
-            int sal1= employee.getSalary();
+//value = "/add-employee" is the html page
+@WebServlet(name = "add", value = "/add")
+public class Add extends HttpServlet {
 
-            //step1 load the driver class
-            Class.forName("oracle.jdbc.driver.OracleDriver");
+    private EmployeeAddSQL emp;
+    private Oraclecon emp1;
 
-            //step2 create  the connection object
-            Connection con=DriverManager.getConnection(
-                    "jdbc:oracle:thin:@localhost:1521:xe","SYSTEM","1234");
-
-            //step3 create the statement object
-            Statement stmt=con.createStatement();
-
-            // 2. Create a statement
-            Statement  myStmt = con.createStatement();
-
-            // 3. Execute SQL query
-            String sql = "insert into Employee "
-                        + " values (1, 'name1' , 'dep1', 1254)";
-
-            String sql1 = "INSERT INTO Employee (ID, NAME, DEPARTMENT, SALARY) VALUES(5, 'Adi','San jose',145)";
-
-
-            myStmt.executeUpdate(sql1);
-
-            //step4 execute query
-            ResultSet rs=stmt.executeQuery("select * from Employee");
-            //System.out.println(rs);
-            while(rs.next()) {
-               // System.out.println("inside while");
-                System.out.println(rs.getInt(1) + " " + rs.getString(2)
-                        + " " + rs.getString(3)
-                        + " " + rs.getInt(4));
-            }
-
-            //step5 close the connection object
-            con.close();
-
-        }catch(Exception e)
-        {
-            System.out.println(e);
-        }
+    public void init() {
+        emp = new EmployeeAddSQL();
     }
 
-    //
-    public static final String INSERT_USERS_SQL = "INSERT INTO users" + "  (id, name, department, salary) VALUES "
-            + " (?, ?, ?, ?);";
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        response.setContentType("text/html");
 
-    protected Connection getConnection(){
-        Connection connection = null;
+        PrintWriter out = response.getWriter();
+        String name = request.getParameter("username");
+        String department = request.getParameter("department");
+        String id = request.getParameter("id");
+        String salary = request.getParameter("salary");
+
+
+        int id1 = Integer.valueOf(id);
+        int salary1 = Integer.valueOf(salary);
+
+        Employee employee = new Employee(id1, name, department, salary1);
         try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            connection = DriverManager.getConnection(
-                    "jdbc:oracle:thin:@localhost:1521:xe","SYSTEM","1234");
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            System.out.println(employee);
+            emp.registerEmployee(employee);
         } catch (ClassNotFoundException e) {
-            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return connection;
+        out.println(name + " " + department + " " + id + " " + salary);
+        out.close();
+
+    }
+    public void destroy() {
     }
 
-    public void insertUser(Employee user) throws SQLException {
-        System.out.println(INSERT_USERS_SQL);
-        // try-with-resource statement will auto close the connection.
-        try (Connection connection = getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
-            preparedStatement.setInt(1, user.getId());
-            preparedStatement.setString(2, user.getName());
-            preparedStatement.setString(3, user.getDepartment());
-            preparedStatement.setInt(4, user.getSalary());
-            System.out.println(preparedStatement);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            printSQLException(e);
-        }
-    }
 
-    private void printSQLException(SQLException e) {
-        System.out.println(e);
-    }
 }
+
--- EmployeeManagement/src/main/java/com/example/EmployeeManagement/EmployeeAddSQL.java
+++ EmployeeManagement/src/main/java/com/example/EmployeeManagement/EmployeeAddSQL.java
@@ -8,27 +8,13 @@ public class EmployeeAddSQL {
 
     public int registerEmployee(Employee employee) throws ClassNotFoundException {
         int result = 0;
-        String INSERT_USERS_SQL = "insert into Employee "
-                + " values (id, name , department, salary)";
-        String INSERT_USERS_SQL1 = "INSERT into Employee (id, name , department, salary) VALUES (?,?,?,?)";
-        System.out.println("printing from employeeadd " + employee);
+        String s = "INSERT INTO SYSTEM.Employee (ID, NAME, DEPARTMENT, SALARY) VALUES (?, ?, ?, ?)";
+
             try{
                 Class.forName("oracle.jdbc.driver.OracleDriver");
-
-                //step2 create  the connection object
                 Connection con=DriverManager.getConnection(
                         "jdbc:oracle:thin:@localhost:1521:xe","SYSTEM","1234");
-
-                Statement myStmt = con.createStatement();
-
-                // 3. Execute SQL query
-                String sql = "insert into Employee "
-                        + " values (employee.getId(), employee.getName() , employee.getDepartment(), employee.getSalary() )";
-
-              //  myStmt.executeUpdate(sql);
-
-                // Step 2:Create a statement using connection object
-                 PreparedStatement preparedStatement = con.prepareStatement(INSERT_USERS_SQL1);
+                 PreparedStatement preparedStatement = con.prepareStatement(s);
 
                     preparedStatement.setInt(1, employee.getId());//1 specifies the first parameter in the query
                     preparedStatement.setString(2, employee.getName());
@@ -36,11 +22,9 @@ public class EmployeeAddSQL {
                     preparedStatement.setInt(4, employee.getSalary());
 
                     System.out.println(preparedStatement);
-                    // Step 3: Execute the query or update query
                     result = preparedStatement.executeUpdate();
 
             } catch (SQLException e) {
-                // process sql exception
                 printSQLException(e);
             }
             return result;
@@ -61,26 +45,4 @@ public class EmployeeAddSQL {
                 }
             }
         }
-/*
-    public static void main(String args[]){
-        try{
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-
-            Connection con= DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","1234");
-
-            PreparedStatement stmt=con.prepareStatement("insert into Employee values(?,?,?,?)");
-            stmt.setInt(1,12);//1 specifies the first parameter in the query
-            stmt.setString(2,"Ratan");
-            stmt.setString(3,"sales");
-            stmt.setInt(4,1200);
-
-            int i=stmt.executeUpdate();
-            System.out.println(i+" records inserted");
-
-            con.close();
-
-        }catch(Exception e){ System.out.println(e);}
-
-    }*/
-
 }
